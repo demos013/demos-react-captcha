fc1fb089a810552ab611a0c8fc3e5ddfa408e8b5
Phuwadit Rojanasitthiphong <phuwadit.r@dv.co.th>
2019-02-28 16:47:20 +0700

    clean code

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  collectCoverage: true,
-  setupFiles: ["jest-canvas-mock"],
-  setupTestFrameworkScriptFile: "./enzyme.config.js"
-};
---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
+import svgr from '@svgr/rollup'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/captcha.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    external(),
+    postcss({
+      modules: true
+    }),
+    url(),
+    svgr(),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: [ 'external-helpers' ]
+    }),
+    resolve(),
+    commonjs({
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+        'src/captcha.js': [ 'DemosCaptcha' ]
+      }
+    })
+  ]
+}
